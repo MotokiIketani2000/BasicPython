7aaaaed404908b8548b8cfc3dbfd41d57723cddc
MotokiIketani2000 <music.motto.2000@gmail.com>
2025-01-28 10:21:29 -0800

    ユークリッド互除法の作成

--- euclid.py
+++ euclid.py
@@ -2,3 +2,10 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+def euculid(a,b):
+    if a / b ==0:
+        return a
+        
+    return euculid(b, a / b)
+
+print(euculid(int(a),int(b)))
