5f3e7d105795b6c6e311e8a37bce5272b5eea911
MotokiIketani2000 <music.motto.2000@gmail.com>
2025-01-28 10:05:20 -0800

    制御構造確認問題の１

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,19 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+a = int(input("aの値を入力: "))
+b = int(input("bの値を入力: "))
 
-# TODO
+# TOD
+
+def prime_number(n):
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(n)):
+        if n % i == 0:
+            return False
+        
+    return True
+
+print(prime_number(a))
+print(prime_number(b))
+    
+
