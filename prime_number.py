d5b63965a63e34c59496d5e7c704635df4f18ce1
MotokiIketani2000 <music.motto.2000@gmail.com>
2025-01-28 15:02:24 -0800

    制御構文の問題１

--- prime_number.py
+++ prime_number.py
@@ -1,19 +1,28 @@
-a = int(input("aの値を入力: "))
-b = int(input("bの値を入力: "))
+a = input("aの値を入力: ")
+b = input("bの値を入力: ")
 
 # TOD
 
-def prime_number(n):
-    if n <= 1:
-        return False
-    
-    for i in range(2, int(n)):
-        if n % i == 0:
-            return False
-        
-    return True
+a = int(a)
+b = int(b)
 
-print(prime_number(a))
-print(prime_number(b))
-    
+if a <= 1:
+    print(f"{a}は素数ではありません。")
+else:
+    is_prime_a = True
+    for i in range(2, int(a**0.5) + 1):
+        if a % i == 0:
+            is_prime_a = False
+            break
+    print(f"{a}は素数です。" if is_prime_a else f"{a}は素数ではありません。")
 
+
+if b <= 1:
+    print(f"{b}は素数ではありません。")
+else:
+    is_prime_b = True
+    for i in range(2, int(b**0.5) + 1):
+        if b % i == 0:
+            is_prime_b = False
+            break
+    print(f"{b}は素数です。" if is_prime_b else f"{b}は素数ではありません。")
