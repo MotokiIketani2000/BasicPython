52484656082c76b761556e9eff725e59cc18205e
MotokiIketani2000 <music.motto.2000@gmail.com>
2025-01-28 14:33:41 -0800

    制御構文の問題2

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -5,20 +5,19 @@ from math import sin
 # -----------
 import math
 
-def trapezoidal_integral():
-    a = 0
-    b = math.pi / 2
-    n = 100
+a = 0
+b = math.pi / 2
+n = 100
     
-    h = (b - a) / n
+h = (b - a) / n
     
-    total = sin(a) + sin(b)
+total = sin(a) + sin(b)
     
-    for i in range(1, n):
-        x = a + i * h
-        total += 2 * sin(x)
+for i in range(1, n):
+    x = a + i * h
+    total += 2 * sin(x)
     
     result = total * h / 2
-    return result
-integral_value = trapezoidal_integral()
-print(f"台形積分による近似値: {integral_value}")
+    
+
+print(f"台形積分による近似値: {result}")
