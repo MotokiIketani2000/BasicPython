45a2a664d7f1baae39d02aa41807e851ff0bcaaf
MotokiIketani2000 <music.motto.2000@gmail.com>
2025-01-28 14:16:09 -0800

    制御構文の問題４

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,12 +1,7 @@
 # TODO
-def machine_epsilon():
-    epsilon = 1.0
-    
-    while 1.0 + epsilon > 1.0:
-        epsilon_last = epsilon
-        epsilon /= 2
-        
-    return epsilon_last
-
-epsilon = machine_epsilon()
+epsilon = 1.0
+while 1.0 + epsilon > 1.0:
+    epsilon_last = epsilon
+    epsilon /= 2
+   
 print(f"マシンイプシロン{epsilon}")
